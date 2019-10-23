c79304dae890a2ea4c3067b82155456e3f90f2a0
jibinmathew69 <jibinmathew.69@gmail.com>
2019-10-23 18:32:49 +0530

    Add more views

--- taskmate/todolist/urls.py
+++ taskmate/todolist/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.todolist, name='todolist')
+    path('', views.todolist, name='todolist'),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about, name='about')
 ]
--- taskmate/todolist/views.py
+++ taskmate/todolist/views.py
@@ -2,5 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def todolist(request):
-    return render(request, 'todolist.html', {})
-    
+    context = {
+        'welcome_text' : 'Welcome to Todolist'
+    }
+    return render(request, 'todolist.html', context)
+
+
+def contact(request):
+    context = {
+        'welcome_text' : 'Welcome to contact us'
+    }
+    return render(request, 'contact.html', context)
+
+def about(request):
+    context = {
+        'welcome_text' : 'Welcome to about us'
+    }
+    return render(request, 'about.html', context)
