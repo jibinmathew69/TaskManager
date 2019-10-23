c096284741e517bcf9915fe4f29ec880c047298c
jibinmathew69 <jibinmathew.69@gmail.com>
2019-10-23 17:52:40 +0530

    Update todolist view

--- taskmate/todolist/views.py
+++ taskmate/todolist/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def todolist(request):
-    return HttpResponse("Welcome to task page")
+    return render(request, 'todolist.html', {})
+    
